ebeaed960311fd383494c07a96cbf8afd53b7eac
LofiLeon <grimmeisen.leon@gmail.com>
2024-04-16 00:24:51 +0200

    chore: redesign GoalDetailView

--- HealthPulse/Views/GoalDetailView/ContentView.swift
+++ HealthPulse/Views/GoalDetailView/ContentView.swift
@@ -12,108 +12,153 @@ enum UnitSelection {
   case kilometers
 }
 
+enum GraphSelection {
+  case circle
+  case chart
+}
+
 struct GoalDetailView: View {
 
     @EnvironmentObject var manager: HealthDataManager
     @ObservedObject var vm: GoalDetailViewViewModel
     @FocusState private var focusItem: Bool
     @State private var selectedUnit: UnitSelection = .miles // Default selection
+    @State private var selectedGraph: GraphSelection = .circle // Default selection
+    let colorOptions: [Color] = [.red, .green, .blue, .orange, .purple]
+    @State private var selectedColor: Color = .red // Initial selected color
 
     @State private var numberString: String = ""
 
     var body: some View {
         NavigationView {
             ScrollView {
-
-                // Goal Information
                 VStack {
-                    
-                    HStack {
-                        Spacer()
-                        HStack{
-                            TextField("Enter Goal", text: $numberString)
-                              .keyboardType(.decimalPad) // Optional: Set keyboard for decimals
-                              .focused($focusItem)
-                              .frame(width: 120)
-                              .padding(5) // Add some padding between text and border
-                              .background(
-                                RoundedRectangle(cornerRadius: 8) // Set corner radius
-                                  .fill(Color.gray.opacity(0.2)) // Set background color and opacity
-                              )
-                              .multilineTextAlignment(.center) // Center the text
-                        }
-                        Spacer()
+                    // Goal Information
+                    VStack {
+                        
                         HStack {
-                            Picker("Unit", selection: $selectedUnit) {
-                                Text("mi").tag(UnitSelection.miles)
-                                Text("km").tag(UnitSelection.kilometers)
-                              }
-                            .pickerStyle(.segmented)
-                            .frame(width: 120)
+                            Spacer()
+                            HStack{
+                                TextField("Enter Goal", text: $numberString)
+                                    .keyboardType(.decimalPad) // Optional: Set keyboard for decimals
+                                    .focused($focusItem)
+                                    .frame(width: 120)
+                                    .padding(5) // Add some padding between text and border
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 8) // Set corner radius
+                                            .fill(Color.gray.opacity(0.2)) // Set background color and opacity
+                                    )
+                                    .multilineTextAlignment(.center) // Center the text
+                            }
+                            Spacer()
+                            HStack {
+                                Picker("Unit", selection: $selectedUnit) {
+                                    Text("mi").tag(UnitSelection.miles)
+                                    Text("km").tag(UnitSelection.kilometers)
+                                }
+                                .pickerStyle(.segmented)
+                                .frame(width: 120)
+                            }
+                            Spacer()
                         }
-                        Spacer()
+                        .padding(.bottom, 10)
+  
+                        HStack(alignment: .center) {
+                            Spacer()
+                            ZStack {
+                                Text("Start")
+                                    .bold()
+                                    .padding(.bottom, 60)
+                                DatePicker("", selection: $vm.selectedHealthGoal.startDate, in: ...vm.selectedHealthGoal.endDate, displayedComponents: .date)
+                                    .onChange(of: vm.selectedHealthGoal.startDate) { _ in vm.updateData() }
+                                    .labelsHidden()
+                            }
+                            Spacer()
+                            ZStack {
+                                Text("End")
+                                    .bold()
+                                    .padding(.bottom, 60)
+                                DatePicker("", selection: $vm.selectedHealthGoal.endDate, in: vm.selectedHealthGoal.startDate...Calendar.current.date(byAdding: .year, value: 10, to: Date())!, displayedComponents: .date)
+                                    .onChange(of: vm.selectedHealthGoal.endDate) { _ in vm.updateData() }
+                                    .labelsHidden()
+                            }
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
                     }
-                    .padding(.bottom, 10)
-
-                    HStack(alignment: .center) {
-                        Spacer()
-                        VStack {
-                            Text("Start")
-                                .bold()
-                            DatePicker("", selection: $vm.selectedHealthGoal.startDate, in: ...vm.selectedHealthGoal.endDate, displayedComponents: .date)
-                                .onChange(of: vm.selectedHealthGoal.startDate) { _ in vm.updateData() }
-                                .labelsHidden()
-                        }
-                        Spacer()
-                        VStack {
-                            Text("End")
-                                .bold()
-                            DatePicker("", selection: $vm.selectedHealthGoal.endDate, in: vm.selectedHealthGoal.startDate...Calendar.current.date(byAdding: .year, value: 10, to: Date())!, displayedComponents: .date)
-                                .onChange(of: vm.selectedHealthGoal.endDate) { _ in vm.updateData() }
-                                .labelsHidden()
-                        }
-                        Spacer()
-                    }
-                    .frame(maxWidth: .infinity)
-                }
-                .padding(.vertical, 20)
-                VStack {
-                    Text("Widget Preview")
-                        .font(.title2)
-                        .bold()
+                    
                     WidgetPreView(healthGoal: vm.selectedHealthGoal)
+                    
+                    VStack {
+                        HStack {
+                            Text("Graph")
+                                .bold()
+                            Spacer()
+                            Picker("Graph", selection: $selectedGraph) {
+                                Text("Chart").tag(GraphSelection.chart)
+                                Text("Circle").tag(GraphSelection.circle)
+                            }
+                            .pickerStyle(.segmented)
+                            .frame(width: 200)
+                        }
+                        .padding(.vertical)
+                        
+                        HStack {
+                            Text("Color")
+                                .bold()
+                            Spacer()
+                            ForEach(colorOptions, id: \.self) { color in
+                                Circle()
+                                    .foregroundColor(color)
+                                    .frame(width: 30, height: 30)
+                                    .overlay(
+                                        Circle()
+                                            .stroke(color, lineWidth: 7) // White stroke for all circles
+                                            .opacity(color == selectedColor ? 1.0 : 0.0) // Hide stroke for non-selected
+                                    )
+                                    .onTapGesture {
+                                        withAnimation {
+                                            selectedColor = color
+                                        }
+                                    }
+                            }
+                        }
+                    }
+                    .padding(.bottom, 200)
+     
+//                    Spacer()
+//                    
+//                    HStack {
+//                        Text(String(format: "%.3f", vm.selectedHealthGoal.doneUnits))
+//                        Text("km")
+//                    }
+//                    Spacer()
+//                    
+//                    VStack {
+//                        HStack {
+//                            Text("goal: ")
+//                            Text(String(format: "%.3f", vm.selectedHealthGoal.goalUnits))
+//                        }
+//                        HStack {
+//                            Text("done: ")
+//                            Text(String(format: "%.3f", vm.selectedHealthGoal.doneUnits))
+//                        }
+//                        HStack {
+//                            Text("ac pr: ")
+//                            Text(String(format: "%.3f", vm.selectedHealthGoal.actualProgress))
+//                        }
+//                        HStack {
+//                            Text("ex pr: ")
+//                            Text(String(format: "%.3f", vm.selectedHealthGoal.expectedProgress))
+//                        }
+//                        
+//                    }
                 }
-                .padding(.vertical)
-                
-                
-                Spacer()
-                
-                HStack {
-                    Text(String(format: "%.3f", vm.selectedHealthGoal.doneUnits))
-                    Text("km")
-                }
-                Spacer()
-                
-                VStack {
-                    HStack {
-                        Text("goal: ")
-                        Text(String(format: "%.3f", vm.selectedHealthGoal.goalUnits))
-                    }
-                    HStack {
-                        Text("done: ")
-                        Text(String(format: "%.3f", vm.selectedHealthGoal.doneUnits))
-                    }
-                    HStack {
-                        Text("ac pr: ")
-                        Text(String(format: "%.3f", vm.selectedHealthGoal.actualProgress))
-                    }
-                    HStack {
-                        Text("ex pr: ")
-                        Text(String(format: "%.3f", vm.selectedHealthGoal.expectedProgress))
-                    }
-
-                }
+                .ignoresSafeArea()
+                .padding(.vertical, 20)
+                .padding(.horizontal, 20)
             }
+            
             .navigationTitle("Your Running Goal")
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
@@ -135,7 +180,6 @@ struct GoalDetailView: View {
             numberString = String(vm.selectedHealthGoal.goalUnits)
             //vm.updateData()
         }
-        .padding()
     }
 }
 
--- HealthPulse/Views/WidgetPreviewView/WidgetPreviewView.swift
+++ HealthPulse/Views/WidgetPreviewView/WidgetPreviewView.swift
@@ -15,6 +15,7 @@ struct CircularProgressView: View {
     let strokeWidth: CGFloat = 15
     
     var body: some View {
+        
         ZStack {
             Circle()
                 .stroke(lineWidth: strokeWidth) // Adjust line width as needed
@@ -41,34 +42,42 @@ struct WidgetPreView: View {
     var healthGoal: HealthGoal
     
     var body: some View {
+        
         VStack {
-            Text("Running Goal")
+            Text("Widget Preview")
+                .font(.title2)
                 .bold()
-            Spacer()
-            CircularProgressView(healthGoal: healthGoal, actualProgressColor: .green, expectedProgressColor: .red)
-                .padding(5)
-            HStack {
-                Text(String(format: "%.1f km", healthGoal.doneUnits - healthGoal.expectedUnits))
+            
+            VStack {
+                Text("Running Goal")
+                    .bold()
                 Spacer()
-                Text(String(format: "%.1f %%", healthGoal.actualProgress * 100))
+                CircularProgressView(healthGoal: healthGoal, actualProgressColor: .green, expectedProgressColor: .red)
+                    .padding(5)
+                HStack {
+                    Text(String(format: "%.1f km", healthGoal.doneUnits - healthGoal.expectedUnits))
+                    Spacer()
+                    Text(String(format: "%.1f %%", healthGoal.actualProgress * 100))
+                }
+                HStack {
+                    Text(String(format: "%.1f", healthGoal.doneUnits))
+                    Text("/")
+                    Text(String(format: "%.0f", healthGoal.goalUnits))
+                    Text("km")
+                }
             }
-            HStack {
-                Text(String(format: "%.1f", healthGoal.doneUnits))
-                Text("/")
-                Text(String(format: "%.0f", healthGoal.goalUnits))
-                Text("km")
-            }
+            .padding()
+            .frame(width: 170, height: 170)
+            .background(Color(.systemBackground)) // Use system background color
+            .cornerRadius(20) // Rounded corners for the widget-like appearance
+            .shadow(color: .gray, radius: 5, x: 0, y: 2) // Shadow for depth
+            .overlay(
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(Color.gray, lineWidth: 1) // Gray outline
+            )
         }
-        .padding()
-        .frame(width: 170, height: 170)
-        .background(Color(.systemBackground)) // Use system background color
-        .cornerRadius(20) // Rounded corners for the widget-like appearance
-        .shadow(color: .gray, radius: 5, x: 0, y: 2) // Shadow for depth
-        .overlay(
-            RoundedRectangle(cornerRadius: 16)
-                .stroke(Color.gray, lineWidth: 1) // Gray outline
-        )
-        .padding()
+        
+
     }
 }
 
