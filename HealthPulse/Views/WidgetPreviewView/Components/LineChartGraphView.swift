f2e4114d692770c43bb826551048736b8c5ed55a
LofiLeon <grimmeisen.leon@gmail.com>
2025-02-03 18:18:29 +0100

    refactor: lineChartGrapgh (+ scale it properly)

--- HealthPulse/Views/WidgetPreviewView/Components/LineChartGraphView.swift
+++ HealthPulse/Views/WidgetPreviewView/Components/LineChartGraphView.swift
@@ -14,54 +14,63 @@ struct LineChartGraphView: View {
     var body: some View {
         VStack {
             ZStack {
-                Chart(healthGoal.data) { dataPoint in
-                    LineMark(
-                        x: .value("Date", dataPoint.date),
-                        y: .value("Units", dataPoint.unitsAcc)
-                    )
-                    .interpolationMethod(.monotone)
-                }
-                .chartXScale(domain: healthGoal.startDate ... healthGoal.endDate)
-                .chartYScale(domain: 0 ... healthGoal.goalUnits)
-                .foregroundColor(.green)
-                
-                Chart(healthGoal.data) { dataPoint in
-                    AreaMark(
-                        x: .value("Date", dataPoint.date),
-                        y: .value("Units", dataPoint.unitsAcc)
-                    )
-                    .interpolationMethod(.monotone)
-                    .foregroundStyle(
-                        LinearGradient(
-                            gradient: Gradient(colors: [.green, .clear]),
-                            startPoint: .top,
-                            endPoint: .bottom
-                        )
-                    )
-                }
-                .chartXScale(domain: healthGoal.startDate ... healthGoal.endDate)
-                .chartYScale(domain: 0 ... healthGoal.goalUnits)
-
-                Chart() {
-                    LineMark(
-                        x: .value("Date", healthGoal.startDate),
-                        y: .value("Units", 0.0)
-                    )
-                    LineMark(
-                        x: .value("Date", healthGoal.endDate),
-                        y: .value("Units", healthGoal.goalUnits)
-                    )
-                    
-                }
-                .chartXScale(domain: healthGoal.startDate ... healthGoal.endDate)
-                .chartYScale(domain: 0 ... healthGoal.goalUnits)
-                .foregroundColor(.gray).opacity(0.5)
-
+                lineChart
+                areaChart
+                goalLineChart
             }
             .chartXAxis(.hidden)
             .chartYAxis(.hidden)
             .chartLegend(.hidden)
         }
+    }
+    
+    private var lineChart: some View {
+        Chart(healthGoal.data) { dataPoint in
+            LineMark(
+                x: .value("Date", dataPoint.date),
+                y: .value("Units", dataPoint.unitsAcc)
+            )
+            .interpolationMethod(.monotone)
+        }
+        .chartXScale(domain: healthGoal.startDate ... healthGoal.endDate)
+        .chartYScale(domain: 0 ... max(healthGoal.goalUnits, healthGoal.doneUnits))
+        .foregroundColor(.green)
+    }
+    
+    private var areaChart: some View {
+        Chart(healthGoal.data) { dataPoint in
+            AreaMark(
+                x: .value("Date", dataPoint.date),
+                y: .value("Units", dataPoint.unitsAcc)
+            )
+            .interpolationMethod(.monotone)
+            .foregroundStyle(
+                LinearGradient(
+                    gradient: Gradient(colors: [.green, .clear]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+        }
+        .chartXScale(domain: healthGoal.startDate ... healthGoal.endDate)
+        .chartYScale(domain: 0 ... max(healthGoal.goalUnits, healthGoal.doneUnits))
+    }
+    
+    private var goalLineChart: some View {
+        Chart() {
+            LineMark(
+                x: .value("Date", healthGoal.startDate),
+                y: .value("Units", 0.0)
+            )
+            LineMark(
+                x: .value("Date", healthGoal.endDate),
+                y: .value("Units", healthGoal.goalUnits)
+            )
+            
+        }
+        .chartXScale(domain: healthGoal.startDate ... healthGoal.endDate)
+        .chartYScale(domain: 0 ... max(healthGoal.goalUnits, healthGoal.doneUnits))
+        .foregroundColor(.gray).opacity(0.5)
     }
 }
 
