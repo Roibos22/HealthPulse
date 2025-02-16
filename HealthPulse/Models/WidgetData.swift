bd1344a775e34382fad0ddfe3ade25c997a8705c
LofiLeon <grimmeisen.leon@gmail.com>
2025-02-16 09:22:18 +0100

    feat: remove unneeded colors

--- HealthPulse/Models/WidgetData.swift
+++ HealthPulse/Models/WidgetData.swift
@@ -12,18 +12,18 @@ enum WidgetColorSet: String, Codable {
     case white
     case gray
     case black
-    case blue
-    case yellow
-    case red
+    //case blue
+    //case yellow
+    //case red
     
     var background: Color {
         switch self {
         case .white: return widgetBackroundColorWhite
         case .gray: return widgetBackroundColorGray
         case .black: return widgetBackroundColorBlack
-        case .blue: return widgetBackroundColorBlue
-        case .yellow: return widgetBackroundColorYellow
-        case .red: return widgetBackroundColorRed
+        //case .blue: return widgetBackroundColorBlue
+        //case .yellow: return widgetBackroundColorYellow
+        //case .red: return widgetBackroundColorRed
         }
     }
     
@@ -32,9 +32,9 @@ enum WidgetColorSet: String, Codable {
         case .white: return widgetForegroundColorBlack
         case .gray: return widgetForegroundColorWhite
         case .black: return widgetForegroundColorWhite
-        case .blue: return widgetForegroundColorBlack
-        case .yellow: return widgetForegroundColorBlack
-        case .red: return widgetForegroundColorBlack
+        //case .blue: return widgetForegroundColorBlack
+        //case .yellow: return widgetForegroundColorBlack
+        //case .red: return widgetForegroundColorBlack
         }
     }
     
@@ -43,9 +43,9 @@ enum WidgetColorSet: String, Codable {
         case .white: return widgetForegroundColorGreen
         case .gray: return widgetForegroundColorGreen
         case .black: return widgetForegroundColorGreen
-        case .blue: return widgetForegroundColorGreen
-        case .yellow: return widgetForegroundColorGreenDark
-        case .red: return widgetForegroundColorGreen
+        //case .blue: return widgetForegroundColorGreen
+        //case .yellow: return widgetForegroundColorGreenDark
+        //case .red: return widgetForegroundColorGreen
         }
     }
     
@@ -54,10 +54,10 @@ enum WidgetColorSet: String, Codable {
         case .white: return widgetForegroundColorRed
         case .gray: return widgetForegroundColorRed
         case .black: return widgetForegroundColorRed
-        case .blue: return widgetForegroundColorRed
-        case .yellow: return widgetForegroundColorRedDark
-        case .red: return widgetBackroundColorYellow
+        //case .blue: return widgetForegroundColorRed
+        //case .yellow: return widgetForegroundColorRedDark
+        //case .red: return widgetBackroundColorYellow
         }
     }
 }
-let widgetColorSets: [WidgetColorSet] = [.white, .gray, .black, .blue, .yellow, .red]
+let widgetColorSets: [WidgetColorSet] = [.white, .gray, .black] //, .blue, .yellow, .red]
